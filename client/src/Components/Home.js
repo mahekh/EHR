03280a7665a872056cab0bd46c6890381ff24321
mahekh <mjh2000@hw.ac.uk>
2023-01-15 21:10:09 +0400

    style

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import Homescreen from "../assets/home1.jpg";
-import Design from "../assets/design.jpg"
 import '../styles/Home.css'
 
 function Home() {
@@ -8,13 +6,17 @@ function Home() {
 
         <div className='main'>
             <div>
-                <img className='homescreen' src='https://img.freepik.com/free-photo/covid-recovery-center-female-doctor-holding-older-patient-s-hands_23-2148847832.jpg?w=2000'/>
+                {/* <img className='homescreen' src='https://img.freepik.com/free-photo/covid-recovery-center-female-doctor-holding-older-patient-s-hands_23-2148847832.jpg?w=2000'/> */}
             </div>
 
             <div className='main__content'>
                 <div className='main__desc'>
                     <p>Electronic Health records</p>
-                    <p>Securing Electronic health records using Blockchain and Web 3.0</p>
+                    <p className='main__desc-quotes'>A technical framework for securing electronic health records on blockchain 
+                            using the web 3.0 approach. With the number of patients rising and the healthcare industry booming, 
+                            there is a pressing need to implement a secure and immutable solution. The primary focus of this project 
+                            is to use the novel technology of Blockchain to provide a decentralized tamper proof ledger which is secure and interoperable
+                    </p>
                 </div>
                 
             </div>
--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../styles/Navbar.css'
 
 
+
 function Navbar() {
     return ( 
 
