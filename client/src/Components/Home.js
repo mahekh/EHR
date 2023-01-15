f8b321c94e6bb3d299c44acf659c5f87edac6618
mahekh <mjh2000@hw.ac.uk>
2023-01-15 16:14:44 +0400

    styling homepage

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import Homescreen from "../assets/home1.jpg";
-
+import Design from "../assets/design.jpg"
+import '../styles/Home.css'
 
 function Home() {
     return (
 
         <div className='main'>
             <div>
-                <img src={Homescreen}/>
+                <img className='homescreen' src='https://img.freepik.com/free-photo/covid-recovery-center-female-doctor-holding-older-patient-s-hands_23-2148847832.jpg?w=2000'/>
             </div>
 
             <div className='main__content'>
                 <div className='main__desc'>
                     <p>Electronic Health records</p>
                     <p>Securing Electronic health records using Blockchain and Web 3.0</p>
-
                 </div>
-
+                
             </div>
+
+            
 
         </div>
     
