3a48e825d4b00fd3d827ee0e6ef82dcf2e4e1bf2
mahekh <mjh2000@hw.ac.uk>
2023-02-04 12:45:39 +0400

    original

--- client/src/Components/AdminDash.js
+++ client/src/Components/AdminDash.js
@@ -3,22 +3,22 @@ import styled from "styled-components";
 import AdminSidebar from './AdminSidebar';
 
 
-function Admin() {
+function AdminDash() {
     return ( 
 
     <div>
         
-       <Container>
         <AdminSidebar/> 
-       </Container>
+       {/* <Container>
+       </Container> */}
     </div>
      );
 }
 
-const Container = styled.div`
-    display: flex;
-    height: 96.5vh;
-    background: linear-gradient(to bottom right, white 60%, #B8E2F2);
-`;
+// const Container = styled.div`
+//     display: flex;
+//     height: 98vh;
+//     background: linear-gradient(to bottom right, white 60%, #B8E2F2);
+// `;
       
-export default Admin;
+export default AdminDash;
--- client/src/Components/PatientDash.js
+++ client/src/Components/PatientDash.js
@@ -1,15 +1,25 @@
 import React from 'react'
+import styled from "styled-components";
+import PatientSidebar from './PatientSidebar';
 
 
 function PatientDash() {
     return ( 
     <>   
         <div>
-           
+        <PatientSidebar/> 
+        {/* <Container>
+       </Container> */}
         </div>
 
     </>     
      );
 }
+
+// const Container = styled.div`
+//     display: flex;
+//     height: 98vh;
+//     background: linear-gradient(to bottom right, white 60%, #B8E2F2);
+// `;
 
 export default PatientDash;
