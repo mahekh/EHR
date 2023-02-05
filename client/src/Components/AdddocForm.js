f52a68e4160aead11ed1ae31ad8f0f752f697707
mahekh <mjh2000@hw.ac.uk>
2023-02-05 20:17:57 +0400

    changesss

--- client/src/Components/AdddocForm.js
+++ client/src/Components/AdddocForm.js
@@ -7,67 +7,25 @@ function AdddocForm() {
       <div class="form-container">
       <form class="register-form">
         
-        <input
-          id="first-name"
-          class="form-field"
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-        />
+        <input id="first-name" class="form-field" type="text" placeholder="First Name" name="firstName"/>
         
-        <input
-          id="last-name"
-          class="form-field"
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-        />
+        <input id="last-name" class="form-field" type="text" placeholder="Last Name" name="lastName"/>
         
-        <input
-          id="email"
-          class="form-field"
-          type="text"
-          placeholder="Email"
-          name="email"
-        />
+        <input id="email" class="form-field" type="text" placeholder="Email" name="email"/>
 
-        <input
-          id="number"
-          class="form-field"
-          type="text"
-          placeholder="Number"
-          name="number"
-        />
+        <input id="number" class="form-field" type="text" placeholder="Number" name="number"/>
 
-        <input
-          id="doc-id"
-          class="form-field"
-          type="text"
-          placeholder="ID"
-          name="id"
-        />
+        <input id="doc-id" class="form-field" type="text" placeholder="ID" name="id"/>
 
-        <input
-          id="city"
-          class="form-field"
-          type="text"
-          placeholder="City"
-          name="city"
-        />
+        <input id="city" class="form-field" type="text" placeholder="City" name="city"/>
 
-        <input
-          id="country"
-          class="form-field"
-          type="text"
-          placeholder="Country"
-          name="country"
-        />
+        <input id="country" class="form-field" type="text" placeholder="Country" name="country"/>
 
         
         <button class="form-field" type="submit">
           Register Doctor
         </button>
-        
+
       </form>
     </div>
     </>
--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import { BiSearch } from "react-icons/bi";
 export default function Navbar(props) {
   return (
     <Nav>
